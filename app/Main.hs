a4bbea229cd36e94ffb39814dac69c965a7f74a5
MarRue <20143778+gpluscb@users.noreply.github.com>
2024-03-28 16:42:01 +0100

    Add some examples

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,36 @@
 module Main where
 
+import qualified Parse
+import qualified Runner
+
 main :: IO ()
 main =
-  putStrLn "Omg Hiii"
+  do
+    x <-
+      mapM
+        (Runner.run Runner.standardIoOperations)
+        ( Parse.parseProgram helloWorldExample
+        )
+    print x
+
+fibExample :: String
+fibExample =
+  "1     \n\
+  \w     \n\
+  \vx# #<\n\
+  \::zv  \n\
+  \pw    \n\
+  \ w   #\n\
+  \ 0x   \n\
+  \ >:< #\n\
+  \> +  ^"
+
+helloWorldExample :: String
+helloWorldExample =
+  "'!'d'l'r'o'w' ','o'l'l'e'H \n\
+  \> > > > > > > > > > > > > P"
+
+infiniteLoopExample :: String
+infiniteLoopExample =
+  "1 \n\
+  \><"
