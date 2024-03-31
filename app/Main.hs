bdc2f6b935ef7aeb0ae93e4e2656cfb2fcb95405
MarRue <20143778+gpluscb@users.noreply.github.com>
2024-03-31 16:20:52 +0200

    More elegant exit code handling and printing

--- app/Main.hs
+++ app/Main.hs
@@ -4,26 +4,42 @@ module Main where
 
 import Data.Maybe (fromMaybe)
 import qualified Parse
+import ProgramData (EndOfProgram (Aborted, Died, Errored, Halted))
 import qualified Runner
 import System.Environment (getArgs)
-import System.Exit (die)
+import qualified System.Exit
 
 main :: IO ()
 main = mainWithArgs =<< getArgs
 
 mainWithArgs :: [String] -> IO ()
-mainWithArgs [path] = execute path Nothing
-mainWithArgs ["--debug", path] = execute path (Just 50_000)
-mainWithArgs _ = die "Usage: exec [--debug] <program file>"
+mainWithArgs [path] = System.Exit.exitWith =<< execute path Nothing
+mainWithArgs ["--debug", path] = System.Exit.exitWith =<< execute path (Just 50_000)
+mainWithArgs _ = System.Exit.die "Usage: exec [--debug] <program file>"
 
-execute :: FilePath -> Maybe Int -> IO ()
+execute :: FilePath -> Maybe Int -> IO System.Exit.ExitCode
 execute path debugMicros = do
   program <- readFile path
-  x <-
+  result <-
     sequence $
       (fromMaybe Runner.runNormal (Runner.runDebug <$> debugMicros))
         <$> (Parse.parseProgram program)
-  print x
+  case result of
+    Left Parse.EmptyLiteral -> do
+      _ <- putStrLn "Parse error: Empty char or number literal"
+      return $ System.Exit.ExitFailure $ -1
+    Right Aborted -> do
+      _ <- putStrLn "Program aborted"
+      return $ System.Exit.ExitFailure $ -2
+    Right Died -> do
+      _ <- putStrLn "Program died without output"
+      return System.Exit.ExitSuccess
+    Right (Halted out) -> do
+      _ <- putStrLn $ "Output: " ++ show out
+      return System.Exit.ExitSuccess
+    Right (Errored e) -> do
+      _ <- putStrLn $ "Error output: " ++ show e
+      return $ System.Exit.ExitFailure e
 
 fibExample :: String
 fibExample =
